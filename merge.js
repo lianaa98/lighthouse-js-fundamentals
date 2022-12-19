ec2ea17fa68ae65ccf6626b7a3a69417d756a05b
lianaa98 <liana.kan98@gmail.com>
2022-12-18 21:13:33 -0800

    finish merge function

---
+++ merge.js
@@ -0,0 +1,9 @@
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
+
+function merge(first, second) {
+  first.push(...second);
+  first.sort();
+  return first;
+}
